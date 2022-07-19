8c2ee203b27713896a03abdd188789653e52608b
Evgeny Zakharov <risenforces@gmail.com>
2022-07-19 18:29:21 +0300

    update

--- ubuntu-18/github-actions-runner-tools.sh
+++ ubuntu-18/github-actions-runner-tools.sh
@@ -3,7 +3,7 @@
 ############
 
 apt-get update
-apt-get install ca-certificates curl gnupg lsb-release
+apt-get install -y ca-certificates curl gnupg lsb-release
 
 ############
 ## Docker ##
@@ -18,7 +18,7 @@ echo \
   $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 apt-get update
-apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
 chmod 666 /var/run/docker.sock
 
 ####################
